aeaeac79b16c6bc7f8e4f9a2c385c8d609db3d6f
ketzacoatl <ketzacoatl@users.noreply.github.com>
2017-03-09 14:53:30 -0500

    use the http client correctly

--- demo/load/load-test.lua
+++ demo/load/load-test.lua
@@ -1,6 +1,9 @@
 cjson = require("cjson")
 hc = require("httpclient").new()
-resp = hc:post('127.0.0.1:8000/', cjson.encode({ hello = "world" }))
+resp = hc:post('http://127.0.0.1:8000/',
+               cjson.encode({ hello = "world" }),
+               {content_type = 'application/json'})
+getr = hc:get('http://127.0.0.1:8000/list')
 if resp.code == 200 then
   print("success!")
 end
@@ -9,3 +12,9 @@ print(resp.code)
 print(resp.headers)
 print(resp.status_line)
 print(resp.err)
+
+print(getr.body)
+print(getr.code)
+print(getr.headers)
+print(getr.status_line)
+print(getr.err)
